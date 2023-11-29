6e9620eb7522e5aeed7eac4d4048e0b7b4d42452
Hans <hanswolden@gmail.com>
2023-11-29 18:24:58 +0100

    add modul06

---
+++ modul06/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-backend-nedlow"
+    storage_account_name = "sabetfsia6kr4exrj"
+    container_name = "tfstate"
+    key = "modul06.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg-test" {
+  name     = "rg-nedlow-test-git"
+  location = "westeurope"
+}
